f3ea60e6782e48d05a83cd06955daabd66c511c7
elsa <elsa.rapon@gmail.com>
2020-05-18 18:59:19 +1000

    working version 1

--- redirectserver.js
+++ redirectserver.js
@@ -7,31 +7,30 @@ const targetBaseUrl = 'https://www.googleapis.com/youtube/v3/search';
 const apiKey = process.env.API_KEY;
 
 function queryyoutube(req, res) {
-  var params = "?" + "q=fitness,cardio,30,min" + "&" + "key=" + apiKey + "&part=id,snippet" + "&type=video" + "&maxResults=10"
+  var params = "?q=" + req.query.q + "&" + "key=" + apiKey + "&part=id,snippet" + "&type=video" + "&maxResults=25"
+  console.log("key = " + apiKey)
   console.log(targetBaseUrl + params)
   fetch(targetBaseUrl + params)
   .then(response => response.json())
   .then(data => {
-    console.log(data)
-    for(i = 0; i < 10; i++) 
-    {
-     console.log(data.items[i].id)
+    const youtubeURL ="https://www.youtube.com/watch?v="
+    var listlink = [youtubeURL + data.items[0].id.videoId];
+    
+    for(i = 1; i < data.items.length; i++) {
+      listlink[i] = youtubeURL + data.items[i].id.videoId;
+    
     }
+    console.log(listlink)
+    const picknumber = Math.floor(Math.random() * listlink.length);
+    const randompick = listlink[picknumber];
+    res.redirect(randompick);
+    console.log(picknumber)
   })
   .catch(err => console.log(err))
 }
 
-function handleRedirect(req, res) {
-  queryyoutube();
-  const targetUrl = targetBaseUrl + req.originalUrl;
-  res.redirect(targetUrl);
-}
-queryyoutube();
-app.get('*', queryyoutube);
+app.get('/video', queryyoutube);
 
 const port = process.env.port || 3000;
-//app.listen(port);
-//for (i = 0; i = 9; i++) {
-  //console.log(data.items[i].id);
-  //{break;}}
-  //+ "&type=video" + "&maxResults=10"
+app.listen(port);
+//http://localhost:3000/video?q=fitness,30min,cardio
