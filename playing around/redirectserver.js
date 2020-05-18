d07a8a14ba4209750195422449e6b0d21d29f6b2
elsa <elsa.rapon@gmail.com>
2020-05-18 12:40:31 +1000

    Add version with loop

--- playing around/redirectserver.js
+++ playing around/redirectserver.js
@@ -1,10 +1,37 @@
 const express = require('express');
+const fetch = require('node-fetch');
+
 const app = express();
-const targetBaseUrl = 'http://youtube.com';
+const targetBaseUrl = 'https://www.googleapis.com/youtube/v3/search';
+
+const apiKey = process.env.API_KEY;
+
+function queryyoutube(req, res) {
+  var params = "?" + "q=fitness,cardio,30,min" + "&" + "key=" + apiKey + "&part=id,snippet" + "&type=video" + "&maxResults=10"
+  console.log(targetBaseUrl + params)
+  fetch(targetBaseUrl + params)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data)
+    for(i = 0; i < 10; i++) 
+    {
+     console.log(data.items[i].id)
+    }
+  })
+  .catch(err => console.log(err))
+}
+
 function handleRedirect(req, res) {
+  queryyoutube();
   const targetUrl = targetBaseUrl + req.originalUrl;
   res.redirect(targetUrl);
 }
-app.get('*', handleRedirect);
+queryyoutube();
+app.get('*', queryyoutube);
+
 const port = process.env.port || 3000;
-app.listen(port);
+//app.listen(port);
+//for (i = 0; i = 9; i++) {
+  //console.log(data.items[i].id);
+  //{break;}}
+  //+ "&type=video" + "&maxResults=10"
